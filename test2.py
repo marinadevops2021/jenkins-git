711ae9eeff21ad387d2b3fb2c09e79a553875185
Marina Minasov <87994268+marinadevops2021@users.noreply.github.com>
2021-08-30 19:27:29 +0300

    comm

--- test2.py
+++ test2.py
@@ -1,4 +1,7 @@
-file=open("data.txt", "a")
-
-file.write("Marina Minasov")
-file.close()
+import os
+if not os.path.exist("data.txt"):
+    file=open("data.txt", "a")
+    file.write("Marina Minasov")
+    file.close()
+else:
+    print("File exist")
